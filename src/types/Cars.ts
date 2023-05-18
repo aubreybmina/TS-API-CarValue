76256443f648be9e9004645a622f48359786255b
Aubrey Mina <aubreybmina@gmail.com>
2023-05-18 14:49:36 +1200

    first commit

---
+++ src/types/Cars.ts
@@ -0,0 +1,17 @@
+export interface Car {
+  id: number
+  model: string
+  year: number
+  value: string
+}
+
+export interface carInput {
+  model: string
+  year: number
+}
+
+export interface TestCase {
+  model: any
+  year: any
+  expected: string
+}
